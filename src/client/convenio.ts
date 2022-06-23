a895f2611ba68a363ce0cfbdb1b21f1835e20c02
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-06-23 16:10:57 -0300

    feat: Implementa métodos de busca para client convenio

--- src/client/convenio.ts
+++ src/client/convenio.ts
@@ -1,5 +1,7 @@
 import { Convenio } from "@/models/convenio";
 import { BaseClient } from "@/client/base-client";
+import { PageRequest } from "@/models/page/page-request";
+import { PageResponse } from "@/models/page/page-response";
 
 /**
  * @author Helcio Laurentino do Carmo Junior
@@ -10,6 +12,16 @@ import { BaseClient } from "@/client/base-client";
 export class ConvenioClient extends BaseClient<Convenio> {
   constructor() {
     super("convenios");
+  }
+
+  public async findConvenioById(id: number): Promise<Convenio> {
+    return this.findById(id);
+  }
+
+  public async findConvenioPaginado(
+    pageRequest: PageRequest
+  ): Promise<PageResponse<Convenio>> {
+    return this.findByFiltrosPaginado(pageRequest);
   }
 
   public async editarConvenio(convenio: Convenio): Promise<void> {
