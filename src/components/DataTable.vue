82a64ab9444a42d087ded97fd40ce176956393ee
Helcio <helcio@carmo.in>
2022-06-26 21:15:12 -0300

    feat: Implementa componente de DataTable

---
+++ src/components/DataTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th v-for="item in header" :key="item">
+                    <abbr title="Position">{{ item }}</abbr>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in data" :key="item">
+                <th>{{ item }}</th>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'DataTable',
+    props: {
+        header: {
+            type: Array,
+            require: true,
+        },
+        data: {
+            type: Array,
+            require: true,
+        },
+    },
+});
+</script>
+
+<style scoped></style>
